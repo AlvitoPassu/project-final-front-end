802c05aeced7359d46b2233b30e28cd4f74b3cee
Timothy Jordy Weley <tmthyjw@gmail.com>
2025-10-30 09:30:00 +0800

    fixing bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import HelloWorld from "./HelloWorld";
+import React from 'react';
+import Header from './components/header'
+import Home from './components/home'
+import Footer  from './components/footer'
 
 const App = () => {
   return (
