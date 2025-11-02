38b2f4b44caa16c69a85982a75eb2160a6edbc5f
Julio Derill Juan Weol <weoljulio03@gmail.com>
2025-11-02 22:53:20 +0800

    repair all generation files

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,8 +4,9 @@ import Generations from './generations'
 const Header = ({ title }) => {
   return (
     <header className="bg-blue-600 text-white py-4 px-6 shadow-md flex justify-between items-center">
+        <h1 className="text-2xl font-bold">{title}</h1>
         <Logo />
-        <Generations />
+        <Generations text= 'Ini Generations'/>
     </header>
   );
 };
